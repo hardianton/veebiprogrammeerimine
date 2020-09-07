ea287eaabeaaa6a480a6c8afa5d66ad0a86cc54f
Hardi Anton <hardianton@users.noreply.github.com>
2020-09-07 17:24:52 +0300

    added semestri algusest on möödunud functionality

--- tund2/home.php
+++ tund2/home.php
@@ -12,6 +12,16 @@ if($hournow >= 6 and $hournow < 8){
 if($hournow >= 8 and $hournow < 18){
 	$partofday = "õppimise aeg";
 }
+
+//jälgime semestri kulgu
+$semesterstart = new DateTime("2020-8-31");
+$semesterend = new DateTime("2020-12-13");
+$semesterduration = $semesterstart->diff($semesterend);
+$today = new DateTime("now");
+$fromsemesterstart = $semesterstart->diff($today);
+//saime aja erinevuse objektina seda niisama näidata ei saa
+$fromsemesterstartdays = $fromsemesterstart->format("%r%a");
+
 ?>
 <!DOCTYPE html>
 <html lang="et">
@@ -24,8 +34,9 @@ if($hournow >= 8 and $hournow < 18){
 <img src="../img/vp_banner.png" alt="Veebiprogrammeerimise kursuse logo">
   <h1><?php echo $username; ?> programmeerib veebi</h1>
   <p>See veebileht on loodud õppetöö kaigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
-  <p> Lehe avamise aeg: <?php echo $fulltimenow; ?>. 
+  <p> Lehe avamise aeg: <?php echo $fulltimenow .", semestri algusest on möödunud " . $fromsemesterstartdays . " päeva"; ?>. 
   <?php echo "Parajasti on " .$partofday ."."; ?></p>
+  
 
 </body>
 </html>
